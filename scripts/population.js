1a4a4123c6d9e681a113447d6389ba4d888b1971
Romain Crestey <rcrestey@gmail.com>
2020-01-31 10:19:24 +0100

    add basic fetch

--- scripts/population.js
+++ scripts/population.js
@@ -1,1 +1,7 @@
 console.log('Good luck space cowboy !');
+
+fetch(
+  'https://raw.githubusercontent.com/iOiurson/formation-dataviz/master/data/population.json',
+)
+  .then(resp => resp.json())
+  .then(data => console.log('DATA', data));
