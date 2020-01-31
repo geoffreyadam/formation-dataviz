fefc6cf22790adba87baef9dbbe92184975a6305
Romain Crestey <rcrestey@gmail.com>
2020-01-31 11:44:08 +0100

    add DOMContentLoaded

--- scripts/pokedex.js
+++ scripts/pokedex.js
@@ -1,7 +1,9 @@
 console.log("Gotta catch'em all !");
 
-fetch(
-  'https://raw.githubusercontent.com/iOiurson/formation-dataviz/master/data/pokedex.json',
-)
-  .then(resp => resp.json())
-  .then(data => console.log('DATA', data));
+document.addEventListener('DOMContentLoaded', () => {
+  fetch(
+    'https://raw.githubusercontent.com/iOiurson/formation-dataviz/master/data/pokedex.json',
+  )
+    .then(resp => resp.json())
+    .then(data => console.log('DATA', data));
+});
--- scripts/population.js
+++ scripts/population.js
@@ -1,7 +1,9 @@
 console.log('Good luck space cowboy !');
 
-fetch(
-  'https://raw.githubusercontent.com/iOiurson/formation-dataviz/master/data/population.json',
-)
-  .then(resp => resp.json())
-  .then(data => console.log('DATA', data));
+document.addEventListener('DOMContentLoaded', () => {
+  fetch(
+    'https://raw.githubusercontent.com/iOiurson/formation-dataviz/master/data/population.json',
+  )
+    .then(resp => resp.json())
+    .then(data => console.log('DATA', data));
+});
